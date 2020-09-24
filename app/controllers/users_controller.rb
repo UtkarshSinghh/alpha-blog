169059f8c11d5a91c8c01511c8f05d937b0320b7
UtkarshSinghh <utkarsh0122@gmail.com>
2020-09-24 16:28:16 +0530

    Added delete users and their associated data option

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :edit, :update]
-  before_action :require_user, only: [:edit, :update]
-  before_action :require_same_user, only: [:edit, :update]
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+  before_action :require_user, only: [:edit, :update, :destroy]
+  before_action :require_same_user, only: [:edit, :update, :destroy]
   before_action :already_signed_up, only: [:new, :create]
 
 
@@ -20,8 +20,6 @@ class UsersController < ApplicationController
   
 
   def edit
-    
-
   end
 
   def update
@@ -43,6 +41,14 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
+
+  def destroy
+    @user.destroy
+    session[:user_id]=nil
+    flash[:notice]= "Account and all associated data has been deleted"
+    redirect_to root_path
+  end
+
 
   private
   def user_params
